00f9b38542b173b76c59d51fe0b7226f2186bf2f
Tom Klajn <91869814+tomkl11@users.noreply.github.com>
2022-11-28 22:58:42 +0100

    Create function.h

---
+++ function.h
@@ -0,0 +1,11 @@
+#ifndef SUDOKU_FUNCTION_H
+#define SUDOKU_FUNCTION_H
+
+#include <stdbool.h>
+#include "gameMatrix.h"
+#include "struct.h"
+
+SudokuGrid createdUserSudokuGrid (SudokuGrid , SudokuGrid ); // verif
+SudokuGrid createGameSudokuGrid (int); // verif
+
+#endif //SUDOKU_FUNCTION_H
