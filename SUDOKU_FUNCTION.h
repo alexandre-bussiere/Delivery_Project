609fa4f3f055d05257c6fedcaedc6276fdfcc3f6
RomainDrouet <103487910+RomainDrouet@users.noreply.github.com>
2022-11-30 10:07:02 +0100

    Create SUDOKU_FUNCTION.h

---
+++ SUDOKU_FUNCTION.h
@@ -0,0 +1,11 @@
+#ifndef SUDOKU_FUNCTION_H
+#define SUDOKU_FUNCTION_H
+
+#include <stdbool.h>
+#include "gameMatrix.h"
+#include "struct.h"
+
+void fillMatrix(int **, int); // verif
+void fillMaskRandom(int **, int, int); // verif
+
+#endif //SUDOKU_FUNCTION_H
