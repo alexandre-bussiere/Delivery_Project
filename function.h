bb8c6ad8c2bfd1868466ae1f1058c3c9c5a91563
aldesse <Bussiere.alexandre@gmail.com>
2022-12-01 08:25:22 +0100

    print change amelio + game start and run

--- function.h
+++ function.h
@@ -2,32 +2,30 @@
 #define SUDOKU_FUNCTION_H
 
 #include <stdbool.h>
-
-#include "struct.h"
-
-
-SudokuGrid createSudoku(int); // verif
-
 #include "gameMatrix.h"
 #include "struct.h"
 
+int securityInputInt(int borneMin, int borneMax); // verif
+
+void printMatrice(SudokuGrid); // verif
+void printMatriceWithMask(SudokuGrid , SudokuGrid ); // verif
+
+SudokuGrid createSudoku(int); // verif
+
 void fillMatrix(int **, int); // verif
 void fillMaskRandom(int **, int, int); // verif
+void fillMaskManual(int **, int, int); // verif
 
 SudokuGrid createdUserSudokuGrid (SudokuGrid , SudokuGrid ); // verif
 SudokuGrid createGameSudokuGrid (int); // verif
-
-
-int securityInputInt(int borneMin, int borneMax); // verif
+SudokuGrid createMaskSudokuGrid (int, int, int); // verif
 
 Coordonnee askAndCheckUserCoordonnee(int); // verif
 void playAMove(SudokuGrid , Coordonnee); // verif
 
 bool isMatriceFull(SudokuGrid ); // verif
+bool is_move_valide(int x, int y,int val,SudokuGrid grille);
 
-void printMatrice(SudokuGrid); // verif
-void printMatriceWithMask(SudokuGrid , SudokuGrid ); // verif
-
-
-
+SudokuGrid play_sudoku(SudokuGrid);
+void Color(int couleurDuTexte, int couleurDeFond);
 #endif //SUDOKU_FUNCTION_H
